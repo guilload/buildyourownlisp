3566b8a854ba6e243f031ede487687cb5615214d
Adrien Guillo <adrien.guillo@radius.com>
2016-11-10 16:23:53 -0800

    Read REPL input with readline

--- repl.c
+++ repl.c
@@ -1,9 +1,9 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
+#include <editline/readline.h>
 
-/* Declare a buffer for user input of size 2048 */
-static char input[2048];
 
 int main(int argc, char** argv) {
 
@@ -14,14 +14,18 @@ int main(int argc, char** argv) {
   /* In a never ending loop */
   while (true) {
 
-    /* Output our prompt */
-    fputs("lispy> ", stdout);
+    /* Output our prompt and get input */
+    char* input = readline("lispy> ");
 
-    /* Read a line of user input of maximum size 2048 */
-    fgets(input, 2048, stdin);
+    /* Add input to history */
+    add_history(input);
 
     /* Echo input back to user */
-    printf("No you're a %s", input);
+    printf("No you're a %s\n", input);
+
+    /* Free retrieved input */
+    free(input);
+
   }
 
   return 0;
